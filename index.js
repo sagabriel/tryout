2cd59eb4f118cb86d5e9ff5e4e9617e2fb7ba675
sagabriel <40653782+sagabriel@users.noreply.github.com>
2018-07-02 12:28:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+'use strict';
 
+// [START functions_pubsub_setup]
+const PubSub = require('@google-cloud/pubsub');
+
+// Instantiates a client
+const pubsub = PubSub();
+// [END functions_pubsub_setup]
+
+const Buffer = require('safe-buffer').Buffer;
 /**
  * This application demonstrates how to perform basic operations on
  * subscriptions with the Google Cloud Pub/Sub API.
@@ -20,29 +29,38 @@
  * For more information, see the README.md under /pubsub and the documentation
  * at https://cloud.google.com/pubsub/docs.
  */
-exports.listSubscriptions = (req, res) => {
-  'use strict';
-  const PubSub = require(`@google-cloud/pubsub`);
-  const pubsub = new PubSub();
+exports.listSubscriptions = (event, callback) => {
+  const pubsubMessage = event.data;
+
+  // We're just going to log the message to prove that it worked!
+  console.log(Buffer.from(pubsubMessage.data, 'base64').toString());
+
+  // Don't forget to call the callback!
+  callback();
+};
+// exports.listSubscriptions = (req, res) => {
+//   'use strict';
+//   const PubSub = require(`@google-cloud/pubsub`);
+//   const pubsub = new PubSub();
   
-  var topic = prompt("From what topic you want to list all the subscriptions?");
-  if (req.body.topic === undefined) {
-    res.status(400).send('No Topic defined!');
-    } 
-  else {
-    // Everything is ok
-  pubsub
-  .topic(topic)
-  .getSubscriptions()
-  .then(results => {
-    const subscriptions = results[0];
-    console.log(`Subscriptions for ${topicName}:`);
-    subscriptions.forEach(subscription => console.log(subscription.name));
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
-  // console.log(req.body.topic);
-  res.status(200).end();
-  }
-}
+//   var topic = prompt("From what topic you want to list all the subscriptions?");
+//   if (req.body.topic === undefined) {
+//     res.status(400).send('No Topic defined!');
+//     } 
+//   else {
+//     // Everything is ok
+//   pubsub
+//   .topic(topic)
+//   .getSubscriptions()
+//   .then(results => {
+//     const subscriptions = results[0];
+//     console.log(`Subscriptions for ${topicName}:`);
+//     subscriptions.forEach(subscription => console.log(subscription.name));
+//   })
+//   .catch(err => {
+//     console.error('ERROR:', err);
+//   });
+//   // console.log(req.body.topic);
+//   res.status(200).end();
+//   }
+// }
